20290ddcd95cc5f74e9a0ad62ba1586316ba618f
Marcelo de Matos Gomez <marcelomgomez@hotmail.com>
2022-07-18 01:43:48 -0300

    Configuração do banco de dados

---
+++ Bank-Account-API/Infrastructure.Data/Database/DatabaseConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Infrastructure.Data.Database
+{
+    [ExcludeFromCodeCoverage]
+    public static class DatabaseConfiguration
+    {
+        public static IEnumerable<T> GetAllData<T>(string filePath)
+        {
+            string jsonString = File.ReadAllText(filePath);
+
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonString);
+        }
+
+        public static void InsertAllData<T>(IEnumerable<T> accounts, string filePath)
+        {
+            string jsonString = JsonConvert.SerializeObject(accounts);
+            jsonString = jsonString.Replace(@"\", "");
+
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
