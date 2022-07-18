4b680a526bdb355c89f31910a89d8ba7c08782ef
Marcelo de Matos Gomez <marcelomgomez@hotmail.com>
2022-07-18 01:44:37 -0300

    Criação dos repositórios na camada de Infrastructure

---
+++ Bank-Account-API/Infrastructure.Data/Repositories/AccountHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.AggregatesModel.BankAccountAggregate;
+using Domain.Consts;
+using Infrastructure.Data.Database;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class AccountHistoryRepository : IAccountHistoryRepository
+    {
+        public void InsertAccountTransaction(AccountHistory transactionHistory)
+        {
+            IEnumerable<AccountHistory> accountHistory = DatabaseConfiguration.GetAllData<AccountHistory>(DatabasePathConst.AccountHistoryPath);
+
+            List<AccountHistory> listAccountHistory = accountHistory.ToList();
+            listAccountHistory.Add(transactionHistory);
+
+            DatabaseConfiguration.InsertAllData(listAccountHistory, DatabasePathConst.AccountHistoryPath);
+        }
+    }
+}
---
+++ Bank-Account-API/Infrastructure.Data/Repositories/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.AggregatesModel.BankAccountAggregate;
+using Domain.Consts;
+using Infrastructure.Data.Database;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public void UpdateAccountAmount(Guid accountId, double accountAmount)
+        {
+            IEnumerable<Account> accounts = DatabaseConfiguration.GetAllData<Account>(DatabasePathConst.AccountPath);
+
+            Account account = accounts.FirstOrDefault(x => x.AccountId == accountId);
+            account.TotalAmount = accountAmount;
+
+            accounts.Select(t => { t = account; return t; })
+                    .Where(x => x.AccountId == accountId)
+                    .ToList();
+
+            DatabaseConfiguration.InsertAllData(accounts, DatabasePathConst.AccountPath);
+        }
+    }
+}
