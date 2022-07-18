703fe5aae598331cd440f530907924e53e45fe9f
Marcelo de Matos Gomez <marcelomgomez@hotmail.com>
2022-07-18 01:46:21 -0300

    Criação das controllers

---
+++ Bank-Account-API/Api/Controllers/TransactionController.cs
@@ -0,0 +1,60 @@
+﻿using Application.Commands.Request;
+using Application.Commands.Response;
+using Application.Queries.Interfaces;
+using Domain.Consts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("Api/Bank/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IStatementQuery _statementQuery;
+
+        public TransactionController(IMediator mediator, IStatementQuery statementQuery)
+        {
+            _mediator = mediator;
+            _statementQuery = statementQuery;
+        }
+
+        [HttpPost("Deposit")]
+        public async Task<ActionResult> Deposit([FromBody] DepositRequest request)
+        {
+            await _mediator.Send(request);
+
+            return Ok();
+        }
+
+        [HttpPost("Withdraw")]
+        public async Task<ActionResult> Withdraw([FromBody] WithdrawRequest request)
+        {
+            await _mediator.Send(request);
+
+            return Ok();
+        }
+
+        [HttpPost("TransferToAccount")]
+        public async Task<ActionResult> TransferToAccount([FromBody] TransferAccountRequest request)
+        {
+            await _mediator.Send(request);
+
+            return Ok();
+        }
+
+        [HttpGet("Statement/{accountId}")]
+        public async Task<ActionResult> Statement(Guid accountId)
+        {
+            StatementResponse response = _statementQuery.GetStatement(accountId);
+
+            if (response != null)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(ProblemDetailConst.BadRequestAccountNotExistError);
+        }
+    }
+}
