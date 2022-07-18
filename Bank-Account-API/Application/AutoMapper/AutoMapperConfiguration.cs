ca88aab04eccfbc4875c65d197bc2bad13e2f6b4
Marcelo de Matos Gomez <marcelomgomez@hotmail.com>
2022-07-18 01:42:16 -0300

    Implementação do AutoMapper

---
+++ Bank-Account-API/Application/AutoMapper/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Application.Commands.Response;
+using AutoMapper;
+using Domain.AggregatesModel.BankAccountAggregate;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Application.AutoMapper
+{
+    [ExcludeFromCodeCoverage]
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<BankAccount, StatementResponse>()
+                .ForMember(dst => dst.ExtractDateUtc, opt => opt.Ignore());
+
+            CreateMap<Account, AccountResponse>();
+
+            CreateMap<AccountHistory, AccountHistoryResponse>();
+        }
+    }
+}
