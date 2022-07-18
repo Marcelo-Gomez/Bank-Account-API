c1e072d54f7448a16d7fa8820df3a05a1f2ea98e
Marcelo de Matos Gomez <marcelomgomez@hotmail.com>
2022-07-18 01:46:50 -0300

    Configurações e ajustes gerais na camada de Api

---
+++ Bank-Account-API/Api/Configurations/DependencyInjectionConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Application.Commands.Request;
+using Application.Helpers;
+using Application.Helpers.Interfaces;
+using Application.Queries;
+using Application.Queries.Interfaces;
+using Application.Validators;
+using Domain.AggregatesModel.BankAccountAggregate;
+using FluentValidation;
+using Infrastructure.Data.Repositories;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Api.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDependencyInjectionConfiguration(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<DepositRequest>, DepositRequestValidator>();
+            services.AddTransient<IValidator<WithdrawRequest>, WithdrawRequestValidator>();
+            services.AddTransient<IValidator<TransferAccountRequest>, TransferAccountRequestValidator>();
+
+            services.AddScoped<IAccountQuery, AccountQuery>();
+            services.AddScoped<IStatementQuery, StatementQuery>();
+
+            services.AddScoped<IAccountHistoryRepository, AccountHistoryRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+
+            services.AddScoped<IAccountOperationsHelper, AccountOperationsHelper>();
+        }
+    }
+}
--- Bank-Account-API/Api/Program.cs
+++ Bank-Account-API/Api/Program.cs
@@ -1,23 +1,52 @@
+using Api.Configurations;
+using Api.Middlewares;
+using Application.Validators;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.OpenApi.Models;
+
+#region Builder
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddMvc()
+                .AddFluentValidation(opt =>
+                {
+                    opt.RegisterValidatorsFromAssemblyContaining<DepositRequestValidator>();
+                });
+
+var assembly = AppDomain.CurrentDomain.Load("Application");
+
+builder.Services.AddMediatR(assembly);
+builder.Services.AddAutoMapper(assembly);
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Stone - API de Conta Bancária", Version = "v1" });
+});
+builder.Services.AddDependencyInjectionConfiguration();
+
+#endregion
+
+#region App
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(opt =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    opt.DocumentTitle = "Stone - API de Conta Bancária";
+    opt.SwaggerEndpoint("../swagger/v1/swagger.json", "Desafio Stone");
+});
 
 app.UseAuthorization();
+
+app.UseExceptionHandlerMiddleware();
 
 app.MapControllers();
 
 app.Run();
+
+#endregion
